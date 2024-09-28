324cdb94c1865d176af38c21fbe9c7ec42ed4218
gbrlb <gbermudezar@gmail.com>
2024-09-28 11:05:34 -0300

    [add] writter.py

---
+++ writer.py
@@ -0,0 +1,46 @@
+import datetime as dt
+from threading import Thread, Semaphore
+
+class Writer:
+    def __init__(self, file_name, separator = ',') -> None:
+        self.sem = Semaphore()
+        self.xs = list()
+        self.ys = list()
+        self.separator = separator
+        self.file = open(file_name, "w")
+        self.file.write('Time,Fx,Fy,Fz\n')
+        self.file.close()
+        self.file = open(file_name, "a")
+
+    def update(self, xs, ys) -> None:
+        # Add x and y to lists
+        self.xs = xs
+        self.ys = ys
+        self.sem.release()
+
+    def run_thread(self) -> None:
+        while True:
+            try:
+                self.sem.acquire()
+                for (x,y) in zip(self.xs, self.ys):
+                    line = str(x) + self.separator + str(y) + '\n'
+                    self.file.write(line)
+                    self.file.flush()
+            except KeyboardInterrupt as e:
+                self.file.close()
+                break
+            except Exception as e:
+                self.file.close()
+                exit(1)
+
+    def run(self) -> None:
+        self.thread = Thread(None, self.run_thread, 'WriterThread', [])
+        self.thread.start()
+
+    def wait(self) -> None:
+        self.thread.join()
+
+if __name__ == '__main__':
+    writer = Writer('test.csv')
+    writer.run()
+    writer.wait()
